e101ddee9172563ebaaf1fda0208bd4cc0449972
Edward Chen <edwardmartyr@outlook.com>
2020-04-15 12:00:35 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='zhruby',
+version='0.1',
+description='Zaonhe Ruby. Output TeX and PDF with Shanghainese ruby from Chinese passages.',
+url='#',
+author='Edward Martyr',
+author_email='edwardmartyr@outlook.com',
+license='MIT',
+packages=setuptools.find_packages()
+)
